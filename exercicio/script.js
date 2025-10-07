f41514bf948c0ce602825f7166934eda4f9499b6
Fabrício Coutinho <fabricioleite.rio@gmail.com>
2025-10-07 16:11:10 -0300

    Add files via upload

--- exercicio/script.js
+++ exercicio/script.js
@@ -1,20 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (document.title.includes('Meu Portfólio')) {
-    }
+document.addEventListener("DOMContentLoaded", () => {
 });
-document.addEventListener('submit', (e) => {
+document.addEventListener("submit", (e) => {
     const form = e.target;
-    if (form.id === 'form-contato') {
+    if (form.id === "form-contato") {
         const nome = form.nome.value.trim();
         const email = form.email.value.trim();
         const assunto = form.assunto.value.trim();
         const mensagem = form.mensagem.value.trim();
 
-        if (!nome || !email || !assunto || !mensagem) {
-            e.preventDefault();
-            alert('Por favor, preencha todos os campos.');
-        } else {
-            alert('Mensagem enviada com sucesso!');
-        }
+        console.log("Dados do formulário:", { nome, email, assunto, mensagem });
+            alert("Formulário enviado com sucesso!");
+        
     }
 });
